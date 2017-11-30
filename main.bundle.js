fb24f01bc1fa80ae1dff41076b5c84d0c07be950
Ricky Amparo <rickyamparo@gmail.com>
2017-11-30 10:14:50 -0700

    extract logic from the ajax call into their own functions

--- main.bundle.js
+++ main.bundle.js
@@ -60,15 +60,18 @@
 	  $.ajax({
 	    type: "GET",
 	    url: "https://serene-sea-75169.herokuapp.com/api/v1/foods"
-	  }).then(function (posts) {
-	    console.log('Hey you hit me!');
-	    posts.forEach(function (post) {
-	      $('table#food-table').append("<tr><td>" + post.name + "</td> <td>" + post.calories + "</td></tr>");
-	    });
-	  }).catch(function (error) {
-	    console.error(error);
-	  });
+	  }).then(appendFoods).catch(errorLog);
 	});
+
+	var appendFoods = function appendFoods(posts) {
+	  posts.forEach(function (post) {
+	    $('table#food-table').append("<tr><td>" + post.name + "</td> <td>" + post.calories + "</td></tr>");
+	  });
+	};
+
+	var errorLog = function errorLog(error) {
+	  console.error(error);
+	};
 
 /***/ }),
 /* 2 */
