1c6e42c4dfcd6fe92714f6ba997474e61af0573a
jayphodges <jayphodges@gmail.com>
2017-12-04 17:19:42 -0700

    Adjusted code after merging master

--- main.bundle.js
+++ main.bundle.js
@@ -85,8 +85,7 @@
 
 
 	// module
-	exports.push([module.id, "* {\n  font-family: Raleway;\n  font-weight: 300\n}\n\ntable {\n    border-collapse: collapse;\n}\n\ntable, th, td {\n    border: 1px solid black;\n}\n\nth {\n  background-color: darkgrey;\n  text-align: left;\n}\n\n\n.calories {\n  text-align: right;\n}\n\n.totals {\n  font-weight: bold;\n  background-color: darkgrey;\n}\n\n.column {\n    float: left;\n    width: 25%;\n}\n\n.column-50 {\n    float: left;\n    width: 50%;\n}\n\n.row {\n  padding-left: 20px;\n}\n\n.row:after {\n    content: \"\";\n    display: table;\n    clear: both;\n}\n", ""]);
-
+	exports.push([module.id, "* {\n  font-family: Raleway;\n  font-weight: 300\n}\n\ntable {\n    border-collapse: collapse;\n}\n\ntable, th, td {\n    border: 1px solid black;\n}\n\nth {\n  background-color: darkgrey;\n  text-align: left;\n}\n\n.calories {\n  text-align: right;\n}\n\n.totals {\n  font-weight: bold;\n  background-color: darkgrey;\n}\n\n.column {\n    float: left;\n    width: 25%;\n}\n\n.column-50 {\n    float: left;\n    width: 50%;\n}\n\n.row {\n  padding-left: 20px;\n}\n\n.row:after {\n    content: \"\";\n    display: table;\n    clear: both;\n}\n", ""]);
 
 	// exports
 
@@ -10753,9 +10752,7 @@
 
 	var $ = __webpack_require__(6);
 	var foodRequest = __webpack_require__(8);
-
-	var url = 'https://serene-sea-75169.herokuapp.com/api/v1/';
-
+	var url = 'http://serene-sea-75169.herokuapp.com/api/v1/';
 
 	var deleteFood = function deleteFood(item, foodUrl) {
 	  fetch(foodUrl, { method: 'DELETE' }).then(function (response) {
@@ -10829,8 +10826,6 @@
 	      setTimeout(function () {
 	        deleteFood(foodId, foodUrl);
 	      }, 300);
-	    } else {
-	      debugger;
 	    }
 	  });
 	});
@@ -10880,7 +10875,7 @@
 
 	var appendFood = function appendFood(response) {
 	  response.forEach(function (data) {
-	    $('tbody#food-table').append('<tr class="fr food-row' + data.id + '"><td class="food-name">' + data.name + '</td> <td>' + data.calories + '</td> <td><i class="fa fa-minus-circle" aria-hidden="true"></i></td></tr>');
+	    $('tbody#food-table').append('<tr class="fr food-row' + data.id + '"><td contenteditable="true" class="food-name name">' + data.name + '</td> <td contenteditable="true" class="calories">' + data.calories + '</td> <td><i class="fa fa-minus-circle" aria-hidden="true"></i></td></tr>');
 	  });
 	};
 
